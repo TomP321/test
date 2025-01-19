Amslup/django

Fixed #35870 -- Replaced blank select choice with accessible text.
Thanks James Scholes for providing feedback on accessible options, Thibaud Colas for running screen reader tests, Carlton Gibson for helping get started.

django/contrib/admin/options.py
@@ -1087,11 +1087,13 @@ class ModelAdmin(BaseModelAdmin):
         actions = self._filter_actions_by_permissions(request, self._get_base_actions())
         return {name: (func, name, desc) for func, name, desc in actions}
 
-    def get_action_choices(self, request, default_choices=models.BLANK_CHOICE_DASH):
+    def get_action_choices(self, request, default_choices=None):
         """
         Return a list of choices for use in a form object.  Each choice is a
         tuple (name, description).
         """
+        if default_choices is None:
+            default_choices = [("", settings.FORMS_DEFAULT_BLANK_CHOICE_LABEL)]
         choices = [] + default_choices
         for func, name, description in self.get_actions(request).values():
             choice = (name, description % model_format_dict(self.opts))

django/db/models/fields/reverse_related.py
@@ -11,12 +11,12 @@ they're the closest concept currently available.
 
 import warnings
 
+from django.conf import settings
 from django.core import exceptions
 from django.utils.deprecation import RemovedInDjango60Warning
 from django.utils.functional import cached_property
 from django.utils.hashable import make_hashable
 
-from . import BLANK_CHOICE_DASH
 from .mixins import FieldCacheMixin
 
 
@@ -175,7 +175,7 @@ class ForeignObjectRel(FieldCacheMixin):
     def get_choices(
         self,
         include_blank=True,
-        blank_choice=BLANK_CHOICE_DASH,
+        blank_choice=None,
         limit_choices_to=None,
         ordering=(),
     ):
@@ -186,6 +186,8 @@ class ForeignObjectRel(FieldCacheMixin):
         Analog of django.db.models.fields.Field.get_choices(), provided
         initially for utilization by RelatedFieldListFilter.
         """
+        if blank_choice is None:
+            blank_choice = [("", settings.FORMS_DEFAULT_BLANK_CHOICE_LABEL)]
         limit_choices_to = limit_choices_to or self.limit_choices_to
         qs = self.related_model._default_manager.complex_filter(limit_choices_to)
         if ordering:

django/forms/models.py
@@ -5,6 +5,7 @@ and database field objects.
 
 from itertools import chain
 
+from django.conf import settings
 from django.core.exceptions import (
     NON_FIELD_ERRORS,
     FieldError,
@@ -1456,7 +1457,7 @@ class ModelChoiceField(ChoiceField):
         self,
         queryset,
         *,
-        empty_label="---------",
+        empty_label="",
         required=True,
         widget=None,
         label=None,
@@ -1483,6 +1484,8 @@ class ModelChoiceField(ChoiceField):
         ):
             self.empty_label = None
         else:
+            if empty_label == "":
+                empty_label = settings.FORMS_DEFAULT_BLANK_CHOICE_LABEL
             self.empty_label = empty_label
         self.queryset = queryset
         self.limit_choices_to = limit_choices_to  # limit the queryset later.

tests/forms_tests/widget_tests/test_radioselect.py
@@ -1,12 +1,13 @@
 import datetime
 
+from django.conf import settings
 from django.forms import ChoiceField, Form, MultiWidget, RadioSelect, TextInput
 from django.test import override_settings
 from django.utils.safestring import mark_safe
 
 from .test_choicewidget import ChoiceWidgetTest
 
-BLANK_CHOICE_DASH = (("", "------"),)
+BLANK_CHOICE = (("", settings.FORMS_DEFAULT_BLANK_CHOICE_LABEL),)
 
 
 class RadioSelectTest(ChoiceWidgetTest):
@@ -16,7 +17,9 @@ class RadioSelectTest(ChoiceWidgetTest):
         html = """
         <div>
           <div>
-            <label><input type="radio" name="beatle" value="">------</label>
+            <label>
+                <input type="radio" name="beatle" value="">- Select an option -
+            </label>
           </div>
           <div>
             <label><input checked type="radio" name="beatle" value="J">John</label>
@@ -32,7 +35,7 @@ class RadioSelectTest(ChoiceWidgetTest):
           </div>
         </div>
         """
-        beatles_with_blank = BLANK_CHOICE_DASH + self.beatles
+        beatles_with_blank = BLANK_CHOICE + self.beatles
         for choices in (beatles_with_blank, dict(beatles_with_blank)):
             with self.subTest(choices):
                 self.check_html(self.widget(choices=choices), "beatle", "J", html=html)
@@ -83,11 +86,13 @@ class RadioSelectTest(ChoiceWidgetTest):
         """
         If value is None, none of the options are selected.
         """
-        choices = BLANK_CHOICE_DASH + self.beatles
+        choices = BLANK_CHOICE + self.beatles
         html = """
         <div>
           <div>
-            <label><input checked type="radio" name="beatle" value="">------</label>
+            <label>
+                <input checked type="radio" name="beatle" value="">- Select an option -
+            </label>
           </div>
           <div>
             <label><input type="radio" name="beatle" value="J">John</label>
@@ -463,11 +468,11 @@ class RadioSelectTest(ChoiceWidgetTest):
 
     def test_render_as_subwidget(self):
         """A RadioSelect as a subwidget of MultiWidget."""
-        choices = BLANK_CHOICE_DASH + self.beatles
+        choices = BLANK_CHOICE + self.beatles
         html = """
         <div>
           <div><label>
-            <input type="radio" name="beatle_0" value="">------</label>
+            <input type="radio" name="beatle_0" value="">- Select an option -</label>
           </div>
           <div><label>
             <input checked type="radio" name="beatle_0" value="J">John</label>

tests/model_forms/test_modelchoicefield.py
@@ -1,6 +1,7 @@
 import datetime
 
 from django import forms
+from django.conf import settings
 from django.core.exceptions import ValidationError
 from django.forms.models import ModelChoiceIterator, ModelChoiceIteratorValue
 from django.forms.widgets import CheckboxSelectMultiple
@@ -24,7 +25,7 @@ class ModelChoiceFieldTests(TestCase):
         self.assertEqual(
             list(f.choices),
             [
-                ("", "---------"),
+                ("", settings.FORMS_DEFAULT_BLANK_CHOICE_LABEL),
                 (self.c1.pk, "Entertainment"),
                 (self.c2.pk, "A test"),
                 (self.c3.pk, "Third"),
@@ -102,7 +103,7 @@ class ModelChoiceFieldTests(TestCase):
         self.assertEqual(
             list(f.choices),
             [
-                ("", "---------"),
+                ("", settings.FORMS_DEFAULT_BLANK_CHOICE_LABEL),
                 (self.c1.pk, "Entertainment"),
                 (self.c2.pk, "A test"),
             ],
@@ -118,7 +119,7 @@ class ModelChoiceFieldTests(TestCase):
         self.assertEqual(
             list(gen_two),
             [
-                ("", "---------"),
+                ("", settings.FORMS_DEFAULT_BLANK_CHOICE_LABEL),
                 (self.c1.pk, "Entertainment"),
                 (self.c2.pk, "A test"),
             ],
@@ -130,7 +131,7 @@ class ModelChoiceFieldTests(TestCase):
         self.assertEqual(
             list(f.choices),
             [
-                ("", "---------"),
+                ("", settings.FORMS_DEFAULT_BLANK_CHOICE_LABEL),
                 (self.c1.pk, "category Entertainment"),
                 (self.c2.pk, "category A test"),
                 (self.c3.pk, "category Third"),
@@ -143,7 +144,7 @@ class ModelChoiceFieldTests(TestCase):
         self.assertEqual(
             list(f.choices),
             [
-                ("", "---------"),
+                ("", settings.FORMS_DEFAULT_BLANK_CHOICE_LABEL),
                 (self.c1.pk, "Entertainment"),
                 (self.c2.pk, "A test"),
                 (self.c3.pk, "Third"),
@@ -154,7 +155,7 @@ class ModelChoiceFieldTests(TestCase):
         self.assertEqual(
             list(f.choices),
             [
-                ("", "---------"),
+                ("", settings.FORMS_DEFAULT_BLANK_CHOICE_LABEL),
                 (self.c1.pk, "Entertainment"),
                 (self.c2.pk, "A test"),
                 (self.c3.pk, "Third"),
@@ -174,6 +175,7 @@ class ModelChoiceFieldTests(TestCase):
         self.assertIs(bool(f.choices), True)
 
     def test_choices_radio_blank(self):
+        blank_choice = [("", settings.FORMS_DEFAULT_BLANK_CHOICE_LABEL)]
         choices = [
             (self.c1.pk, "Entertainment"),
             (self.c2.pk, "A test"),
@@ -190,7 +192,7 @@ class ModelChoiceFieldTests(TestCase):
                     )
                     self.assertEqual(
                         list(f.choices),
-                        [("", "---------")] + choices if blank else choices,
+                        (blank_choice + choices if blank else choices),
                     )
 
     def test_deepcopies_widget(self):
@@ -425,7 +427,7 @@ class ModelChoiceFieldTests(TestCase):
         self.assertCountEqual(
             list(f.choices),
             [
-                ("", "---------"),
+                ("", settings.FORMS_DEFAULT_BLANK_CHOICE_LABEL),
                 (self.c1.pk, "Entertainment"),
                 (self.c2.pk, "A test"),
                 (self.c3.pk, "Third"),

TomP321/test

django/contrib/admin/options.py
@@ -1046,11 +1046,13 @@ class ModelAdmin(BaseModelAdmin):
         actions = self._filter_actions_by_permissions(request, self._get_base_actions())
         return {name: (func, name, desc) for func, name, desc in actions}
 
-    def get_action_choices(self, request, default_choices=models.BLANK_CHOICE_DASH):
+    def get_action_choices(self, request, default_choices=None):
         """
         Return a list of choices for use in a form object.  Each choice is a
         tuple (name, description).
         """
+        if default_choices is None:
+            default_choices = [("", settings.BLANK_CHOICE_LABEL)]
         choices = [] + default_choices
         for func, name, description in self.get_actions(request).values():
             choice = (name, description % model_format_dict(self.opts))

django/db/models/fields/reverse_related.py
@@ -9,11 +9,11 @@ They also act as reverse fields for the purposes of the Meta API because
 they're the closest concept currently available.
 """
 
+from django.conf import settings
 from django.core import exceptions
 from django.utils.functional import cached_property
 from django.utils.hashable import make_hashable
 
-from . import BLANK_CHOICE_DASH
 from .mixins import FieldCacheMixin
 
 
@@ -172,7 +172,7 @@ class ForeignObjectRel(FieldCacheMixin):
     def get_choices(
         self,
         include_blank=True,
-        blank_choice=BLANK_CHOICE_DASH,
+        blank_choice=None,
         limit_choices_to=None,
         ordering=(),
     ):
@@ -183,6 +183,8 @@ class ForeignObjectRel(FieldCacheMixin):
         Analog of django.db.models.fields.Field.get_choices(), provided
         initially for utilization by RelatedFieldListFilter.
         """
+        if blank_choice is None:
+            blank_choice = [("", settings.BLANK_CHOICE_LABEL)]
         limit_choices_to = limit_choices_to or self.limit_choices_to
         qs = self.related_model._default_manager.complex_filter(limit_choices_to)
         if ordering:

django/forms/models.py
@@ -5,6 +5,7 @@ and database field objects.
 
 from itertools import chain
 
+from django.conf import settings
 from django.core.exceptions import (
     NON_FIELD_ERRORS,
     FieldError,
@@ -1456,7 +1457,7 @@ class ModelChoiceField(ChoiceField):
         self,
         queryset,
         *,
-        empty_label="---------",
+        empty_label="",
         required=True,
         widget=None,
         label=None,
@@ -1483,6 +1484,8 @@ class ModelChoiceField(ChoiceField):
         ):
             self.empty_label = None
         else:
+            if empty_label == "":
+                empty_label = settings.BLANK_CHOICE_LABEL
             self.empty_label = empty_label
         self.queryset = queryset
         self.limit_choices_to = limit_choices_to  # limit the queryset later.

tests/forms_tests/widget_tests/test_radioselect.py
@@ -1,12 +1,13 @@
 import datetime
 
+from django.conf import settings
 from django.forms import ChoiceField, Form, MultiWidget, RadioSelect, TextInput
 from django.test import override_settings
 from django.utils.safestring import mark_safe
 
 from .test_choicewidget import ChoiceWidgetTest
 
-BLANK_CHOICE_DASH = (("", "------"),)
+BLANK_CHOICE = (("", settings.BLANK_CHOICE_LABEL),)
 
 
 class RadioSelectTest(ChoiceWidgetTest):
@@ -16,7 +17,9 @@ class RadioSelectTest(ChoiceWidgetTest):
         html = """
         <div>
           <div>
-            <label><input type="radio" name="beatle" value="">------</label>
+            <label>
+                <input type="radio" name="beatle" value="">- Select an option -
+            </label>
           </div>
           <div>
             <label><input checked type="radio" name="beatle" value="J">John</label>
@@ -32,7 +35,7 @@ class RadioSelectTest(ChoiceWidgetTest):
           </div>
         </div>
         """
-        beatles_with_blank = BLANK_CHOICE_DASH + self.beatles
+        beatles_with_blank = BLANK_CHOICE + self.beatles
         for choices in (beatles_with_blank, dict(beatles_with_blank)):
             with self.subTest(choices):
                 self.check_html(self.widget(choices=choices), "beatle", "J", html=html)
@@ -83,11 +86,13 @@ class RadioSelectTest(ChoiceWidgetTest):
         """
         If value is None, none of the options are selected.
         """
-        choices = BLANK_CHOICE_DASH + self.beatles
+        choices = BLANK_CHOICE + self.beatles
         html = """
         <div>
           <div>
-            <label><input checked type="radio" name="beatle" value="">------</label>
+            <label>
+                <input checked type="radio" name="beatle" value="">- Select an option -
+            </label>
           </div>
           <div>
             <label><input type="radio" name="beatle" value="J">John</label>
@@ -463,11 +468,11 @@ class RadioSelectTest(ChoiceWidgetTest):
 
     def test_render_as_subwidget(self):
         """A RadioSelect as a subwidget of MultiWidget."""
-        choices = BLANK_CHOICE_DASH + self.beatles
+        choices = BLANK_CHOICE + self.beatles
         html = """
         <div>
           <div><label>
-            <input type="radio" name="beatle_0" value="">------</label>
+            <input type="radio" name="beatle_0" value="">- Select an option -</label>
           </div>
           <div><label>
             <input checked type="radio" name="beatle_0" value="J">John</label>

tests/model_forms/test_modelchoicefield.py
@@ -1,6 +1,7 @@
 import datetime
 
 from django import forms
+from django.conf import settings
 from django.core.exceptions import ValidationError
 from django.forms.models import ModelChoiceIterator, ModelChoiceIteratorValue
 from django.forms.widgets import CheckboxSelectMultiple
@@ -24,7 +25,7 @@ class ModelChoiceFieldTests(TestCase):
         self.assertEqual(
             list(f.choices),
             [
-                ("", "---------"),
+                ("", settings.BLANK_CHOICE_LABEL),
                 (self.c1.pk, "Entertainment"),
                 (self.c2.pk, "A test"),
                 (self.c3.pk, "Third"),
@@ -102,7 +103,7 @@ class ModelChoiceFieldTests(TestCase):
         self.assertEqual(
             list(f.choices),
             [
-                ("", "---------"),
+                ("", settings.BLANK_CHOICE_LABEL),
                 (self.c1.pk, "Entertainment"),
                 (self.c2.pk, "A test"),
             ],
@@ -118,7 +119,7 @@ class ModelChoiceFieldTests(TestCase):
         self.assertEqual(
             list(gen_two),
             [
-                ("", "---------"),
+                ("", settings.BLANK_CHOICE_LABEL),
                 (self.c1.pk, "Entertainment"),
                 (self.c2.pk, "A test"),
             ],
@@ -130,7 +131,7 @@ class ModelChoiceFieldTests(TestCase):
         self.assertEqual(
             list(f.choices),
             [
-                ("", "---------"),
+                ("", settings.BLANK_CHOICE_LABEL),
                 (self.c1.pk, "category Entertainment"),
                 (self.c2.pk, "category A test"),
                 (self.c3.pk, "category Third"),
@@ -143,7 +144,7 @@ class ModelChoiceFieldTests(TestCase):
         self.assertEqual(
             list(f.choices),
             [
-                ("", "---------"),
+                ("", settings.BLANK_CHOICE_LABEL),
                 (self.c1.pk, "Entertainment"),
                 (self.c2.pk, "A test"),
                 (self.c3.pk, "Third"),
@@ -154,7 +155,7 @@ class ModelChoiceFieldTests(TestCase):
         self.assertEqual(
             list(f.choices),
             [
-                ("", "---------"),
+                ("", settings.BLANK_CHOICE_LABEL),
                 (self.c1.pk, "Entertainment"),
                 (self.c2.pk, "A test"),
                 (self.c3.pk, "Third"),
@@ -174,6 +175,7 @@ class ModelChoiceFieldTests(TestCase):
         self.assertIs(bool(f.choices), True)
 
     def test_choices_radio_blank(self):
+        blank_choice = [("", settings.BLANK_CHOICE_LABEL)]
         choices = [
             (self.c1.pk, "Entertainment"),
             (self.c2.pk, "A test"),
@@ -190,7 +192,7 @@ class ModelChoiceFieldTests(TestCase):
                     )
                     self.assertEqual(
                         list(f.choices),
-                        [("", "---------")] + choices if blank else choices,
+                        (blank_choice + choices if blank else choices),
                     )
 
     def test_deepcopies_widget(self):
@@ -425,7 +427,7 @@ class ModelChoiceFieldTests(TestCase):
         self.assertCountEqual(
             list(f.choices),
             [
-                ("", "---------"),
+                ("", settings.BLANK_CHOICE_LABEL),
                 (self.c1.pk, "Entertainment"),
                 (self.c2.pk, "A test"),
                 (self.c3.pk, "Third"),
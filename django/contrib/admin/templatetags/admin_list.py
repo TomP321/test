django/django

Refs #36175 -- Refactor admin to use querystring template tag.

django/contrib/admin/templatetags/admin_list.py
@@ -20,6 +20,7 @@ from django.core.exceptions import ObjectDoesNotExist
 from django.db import models
 from django.db.models.constants import LOOKUP_SEP
 from django.template import Library
+from django.template.defaulttags import querystring
 from django.template.loader import get_template
 from django.templatetags.static import static
 from django.urls import NoReverseMatch
@@ -46,7 +47,7 @@ def paginator_number(cl, i):
     else:
         return format_html(
             '<a href="{}"{}>{}</a> ',
-            cl.get_query_string({PAGE_VAR: i}),
+            querystring(None, cl.query_dict, **{PAGE_VAR: i}),
             mark_safe(' class="end"' if i == cl.paginator.num_pages else ""),
             i,
         )
@@ -64,7 +65,8 @@ def pagination(cl):
     return {
         "cl": cl,
         "pagination_required": pagination_required,
-        "show_all_url": need_show_all_link and cl.get_query_string({ALL_VAR: ""}),
+        "show_all_url": need_show_all_link
+        and querystring(None, cl.query_dict, **{ALL_VAR: ""}),
         "page_range": page_range,
         "ALL_VAR": ALL_VAR,
         "1": 1,
@@ -169,9 +171,15 @@ def result_headers(cl):
             "sorted": is_sorted,
             "ascending": order_type == "asc",
             "sort_priority": sort_priority,
-            "url_primary": cl.get_query_string({ORDER_VAR: ".".join(o_list_primary)}),
-            "url_remove": cl.get_query_string({ORDER_VAR: ".".join(o_list_remove)}),
-            "url_toggle": cl.get_query_string({ORDER_VAR: ".".join(o_list_toggle)}),
+            "url_primary": querystring(
+                None, cl.query_dict, **{ORDER_VAR: ".".join(o_list_primary)}
+            ),
+            "url_remove": querystring(
+                None, cl.query_dict, **{ORDER_VAR: ".".join(o_list_remove)}
+            ),
+            "url_toggle": querystring(
+                None, cl.query_dict, **{ORDER_VAR: ".".join(o_list_toggle)}
+            ),
             "class_attrib": (
                 format_html(' class="{}"', " ".join(th_classes)) if th_classes else ""
             ),
@@ -386,7 +394,9 @@ def date_hierarchy(cl):
         day_lookup = cl.params.get(day_field)
 
         def link(filters):
-            return cl.get_query_string(filters, [field_generic])
+            params = {field_generic: None}
+            params.update(filters)
+            return querystring(None, cl.query_dict, **params)
 
         if not (year_lookup or month_lookup or day_lookup):
             # select appropriate start level

django/contrib/admin/views/main.py
@@ -31,6 +31,7 @@ from django.core.paginator import InvalidPage
 from django.db.models import F, Field, ManyToOneRel, OrderBy
 from django.db.models.constants import LOOKUP_SEP
 from django.db.models.expressions import Combinable
+from django.template.defaulttags import querystring
 from django.urls import reverse
 from django.utils.http import urlencode
 from django.utils.timezone import make_aware
@@ -125,15 +126,19 @@ class ChangeList:
             )
         self.to_field = to_field
         self.params = dict(request.GET.items())
-        self.filter_params = dict(request.GET.lists())
+        self.query_dict = request.GET.copy()
         if PAGE_VAR in self.params:
             del self.params[PAGE_VAR]
-            del self.filter_params[PAGE_VAR]
+            del self.query_dict[PAGE_VAR]
         if ERROR_FLAG in self.params:
             del self.params[ERROR_FLAG]
-            del self.filter_params[ERROR_FLAG]
-        self.remove_facet_link = self.get_query_string(remove=[IS_FACETS_VAR])
-        self.add_facet_link = self.get_query_string({IS_FACETS_VAR: True})
+            del self.query_dict[ERROR_FLAG]
+        self.remove_facet_link = querystring(
+            None, self.query_dict, **{IS_FACETS_VAR: None}
+        )
+        self.add_facet_link = querystring(
+            None, self.query_dict, **{IS_FACETS_VAR: True}
+        )
 
         if self.is_popup:
             self.list_editable = ()
@@ -161,7 +166,7 @@ class ChangeList:
         """
         Return all params except IGNORED_PARAMS.
         """
-        params = params or self.filter_params
+        params = params or dict(self.query_dict.lists())
         lookup_params = params.copy()  # a dictionary of the query string
         # Remove all the parameters that are globally and systematically
         # ignored.
@@ -279,7 +284,7 @@ class ChangeList:
             new_params = {}
         if remove is None:
             remove = []
-        p = self.filter_params.copy()
+        p = self.query_dict.copy()
         for r in remove:
             for k in list(p):
                 if k.startswith(r):
@@ -574,9 +579,12 @@ class ChangeList:
         )
 
         # Set query string for clearing all filters.
-        self.clear_all_filters_qs = self.get_query_string(
-            new_params=remaining_lookup_params,
-            remove=self.get_filters_params(),
+        params = {param: None for param in self.get_filters_params()}
+        params.update(remaining_lookup_params)
+        self.clear_all_filters_qs = querystring(
+            None,
+            self.query_dict,
+            **params,
         )
         # Remove duplicates from results, if necessary
         if filters_may_have_duplicates | search_may_have_duplicates:

TomP321/test

django/contrib/admin/templatetags/admin_list.py
@@ -20,6 +20,7 @@ from django.core.exceptions import ObjectDoesNotExist
 from django.db import models
 from django.db.models.constants import LOOKUP_SEP
 from django.template import Library
+from django.template.defaulttags import querystring
 from django.template.loader import get_template
 from django.templatetags.static import static
 from django.urls import NoReverseMatch
@@ -46,7 +47,7 @@ def paginator_number(cl, i):
     else:
         return format_html(
             '<a href="{}"{}>{}</a> ',
-            cl.get_query_string({PAGE_VAR: i}),
+            querystring(None, cl.filter_params, **{PAGE_VAR: i}),
             mark_safe(' class="end"' if i == cl.paginator.num_pages else ""),
             i,
         )
@@ -64,7 +65,8 @@ def pagination(cl):
     return {
         "cl": cl,
         "pagination_required": pagination_required,
-        "show_all_url": need_show_all_link and cl.get_query_string({ALL_VAR: ""}),
+        "show_all_url": need_show_all_link
+        and querystring(None, cl.filter_params, **{ALL_VAR: ""}),
         "page_range": page_range,
         "ALL_VAR": ALL_VAR,
         "1": 1,
@@ -169,9 +171,15 @@ def result_headers(cl):
             "sorted": is_sorted,
             "ascending": order_type == "asc",
             "sort_priority": sort_priority,
-            "url_primary": cl.get_query_string({ORDER_VAR: ".".join(o_list_primary)}),
-            "url_remove": cl.get_query_string({ORDER_VAR: ".".join(o_list_remove)}),
-            "url_toggle": cl.get_query_string({ORDER_VAR: ".".join(o_list_toggle)}),
+            "url_primary": querystring(
+                None, cl.filter_params, **{ORDER_VAR: ".".join(o_list_primary)}
+            ),
+            "url_remove": querystring(
+                None, cl.filter_params, **{ORDER_VAR: ".".join(o_list_remove)}
+            ),
+            "url_toggle": querystring(
+                None, cl.filter_params, **{ORDER_VAR: ".".join(o_list_toggle)}
+            ),
             "class_attrib": (
                 format_html(' class="{}"', " ".join(th_classes)) if th_classes else ""
             ),
@@ -386,7 +394,9 @@ def date_hierarchy(cl):
         day_lookup = cl.params.get(day_field)
 
         def link(filters):
-            return cl.get_query_string(filters, [field_generic])
+            params = {field_generic: None}
+            params.update(filters)
+            return querystring(None, cl.filter_params, **params)
 
         if not (year_lookup or month_lookup or day_lookup):
             # select appropriate start level

django/contrib/admin/views/main.py
@@ -1,3 +1,4 @@
+import warnings
 from datetime import datetime, timedelta
 
 from django import forms
@@ -31,8 +32,9 @@ from django.core.paginator import InvalidPage
 from django.db.models import F, Field, ManyToOneRel, OrderBy
 from django.db.models.constants import LOOKUP_SEP
 from django.db.models.expressions import Combinable
+from django.template.defaulttags import querystring
 from django.urls import reverse
-from django.utils.http import urlencode
+from django.utils.deprecation import RemovedInDjango70Warning
 from django.utils.timezone import make_aware
 from django.utils.translation import gettext
 
@@ -125,15 +127,19 @@ class ChangeList:
             )
         self.to_field = to_field
         self.params = dict(request.GET.items())
-        self.filter_params = dict(request.GET.lists())
+        self.filter_params = request.GET.copy()
         if PAGE_VAR in self.params:
             del self.params[PAGE_VAR]
             del self.filter_params[PAGE_VAR]
         if ERROR_FLAG in self.params:
             del self.params[ERROR_FLAG]
             del self.filter_params[ERROR_FLAG]
-        self.remove_facet_link = self.get_query_string(remove=[IS_FACETS_VAR])
-        self.add_facet_link = self.get_query_string({IS_FACETS_VAR: True})
+        self.remove_facet_link = querystring(
+            None, self.filter_params, **{IS_FACETS_VAR: None}
+        )
+        self.add_facet_link = querystring(
+            None, self.filter_params, **{IS_FACETS_VAR: True}
+        )
 
         if self.is_popup:
             self.list_editable = ()
@@ -161,7 +167,7 @@ class ChangeList:
         """
         Return all params except IGNORED_PARAMS.
         """
-        params = params or self.filter_params
+        params = params or dict(self.filter_params.lists())
         lookup_params = params.copy()  # a dictionary of the query string
         # Remove all the parameters that are globally and systematically
         # ignored.
@@ -275,6 +281,14 @@ class ChangeList:
             raise IncorrectLookupParameters(e) from e
 
     def get_query_string(self, new_params=None, remove=None):
+        from django.utils.http import urlencode
+
+        warnings.warn(
+            "ChangeList.get_query_string() is deprecated. "
+            "use django.template.defaulttags.querystring instead.",
+            RemovedInDjango70Warning,
+            stacklevel=2,
+        )
         if new_params is None:
             new_params = {}
         if remove is None:
@@ -574,9 +588,12 @@ class ChangeList:
         )
 
         # Set query string for clearing all filters.
-        self.clear_all_filters_qs = self.get_query_string(
-            new_params=remaining_lookup_params,
-            remove=self.get_filters_params(),
+        params = {param: None for param in self.get_filters_params()}
+        params.update(remaining_lookup_params)
+        self.clear_all_filters_qs = querystring(
+            None,
+            self.filter_params,
+            **params,
         )
         # Remove duplicates from results, if necessary
         if filters_may_have_duplicates | search_may_have_duplicates:
aec89bd50259b20b129388161aa13aaec9d9277b
GappleBee <irrationalmathematicspro7@gmail.com>
2025-03-04 21:43:27 +0000

    Reordered search.py imports.

--- django/contrib/postgres/search.py
+++ django/contrib/postgres/search.py
@@ -1,5 +1,3 @@
-from psycopg2.extensions import adapt
-
 import re
 
 from django.db.models import (
@@ -14,6 +12,8 @@ from django.db.models import (
 )
 from django.db.models.expressions import CombinedExpression, register_combinable_fields
 from django.db.models.functions import Cast, Coalesce
+
+from psycopg2.extensions import adapt
 
 
 _SEARCH_SPEC_CHARS = r"['\0\[\]()|&:*!@<>\\]"
